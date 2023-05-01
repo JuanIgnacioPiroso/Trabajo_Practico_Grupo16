fb9b0dec35917a5e5e7137d22b7eb684a2655763
Juan Ignacio <80292010+JuanIgnacioPiroso@users.noreply.github.com>
2023-05-01 13:48:32 -0300

    Se crea la data elemento

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,11 @@
 data Grupo = Metal | NoMetal | Halogeno | GasNoble deriving (Show, Eq)
 
-
+data Elemento = Elemento {
+    nombre::String,
+    simboloQ :: String,
+    nAtomico :: Int,
+    grupoE :: Grupo
+} 
 
 
 
